499e012b233af166e792645618dafe1a2b8ab9e9
Rollonmath42 <cmorales9016@gmail.com>
2021-11-22 14:14:34 -0600

    added header on webserver due to error on frontend breaking api lookups

--- webserver.js
+++ webserver.js
@@ -9,6 +9,11 @@ app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/images", express.static(__dirname + "/images"));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+})
+
 app.get("/", (request, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
